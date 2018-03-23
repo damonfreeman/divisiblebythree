ecd9568c92ce065706d97b99ae524d82d301fedd
Damon Freeman <damonefreeman@gmail.com>
2018-03-23 01:25:10 -0500

    Added logic in case user entered a number too large

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,19 @@ function byThree2() {
 }
 
 function byThree1() {
-  var yourNum = prompt("Type in the number 100 and click OK",);
+  var yourNum = prompt("Type in any number 100 and click OK",);
   var myNumb = prompt("Type in any number 3 or 5 or 7 to see which numbers 100 are divisible by",);
-
-  for(var i = 1; i < yourNum; i++) {
-    if(i % myNumb != 0) {
-      document.getElementById("numberfield").innerHTML += i + " ";
-  } else {
-      document.getElementById("numberfield").innerHTML += "MATCH ";
-  }
+  if(yourNum <= 100) {
+    for(var i = 1; i < yourNum; i++) {
+      if(i % myNumb != 0) {
+        document.getElementById("numberfield").innerHTML += i + " ";
+    } else {
+        document.getElementById("numberfield").innerHTML += "MATCH ";
+    } 
+  } 
+  
+    } else {
+      document.getElementById("numberfield").innerHTML += "Enter a lower base number between 1 and 100. Then enter another that your first number should be divided by ";
     }
     
 }
