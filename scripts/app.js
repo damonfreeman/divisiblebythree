826f73dc982d14dd912da65c96be423a432feaec
Damon Freeman <damonefreeman@gmail.com>
2018-03-23 01:11:08 -0500

    Add option to include any divisible number

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
+
 function byThree2() {
   document.getElementById("numberfield").innerHTML = "";
 }
 
 function byThree1() {
   var yourNum = prompt("Type in the number 100 and click OK",);
+  var myNumb = prompt("Type in any number 3 or 5 or 7 to see which numbers 100 are divisible by",);
+
   for(var i = 1; i < yourNum; i++) {
-    if(i % 3 != 0) {
+    if(i % myNumb != 0) {
       document.getElementById("numberfield").innerHTML += i + " ";
   } else {
       document.getElementById("numberfield").innerHTML += "MATCH ";
